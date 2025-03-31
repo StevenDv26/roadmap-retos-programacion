c1e7f3df90aa3e50c63c085f32402eeb8b273d51
StevenDv26 <steveb05yt@gmail.com>
2025-03-31 13:53:48 -0500

    Create StevenDv26.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/StevenDv26.py
@@ -0,0 +1,24 @@
+# https://python.org/
+
+# comentario en una linea
+
+"""
+esto segun
+es otro tipo 
+de comentario
+en varias lineas
+"""
+
+'''
+esto segun
+es otro tipo 
+de comentario
+en varias lineas
+'''
+
+my_variable = "Mi variable"
+my_variable = "Esta seri mi otra bariable"
+
+MY_CONSTANT = "3.22222"
+MY_CONSTANT = ""
+
